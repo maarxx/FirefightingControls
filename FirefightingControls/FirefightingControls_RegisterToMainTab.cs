1ce0247a80e7c50f8c4d6082e1dab6929fb231b6
maarxx <chris.lewark@gmail.com>
2020-12-11 23:51:15 -0500

    Initial Implementation

---
+++ FirefightingControls/FirefightingControls_RegisterToMainTab.cs
@@ -0,0 +1,67 @@
+﻿using ModButtons;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace FirefightingControls
+{
+    class FirefightingControls_RegisterToMainTab
+    {
+        private static MapComponent_FirefightingControls getComponent()
+        {
+            return Find.CurrentMap.GetComponent<MapComponent_FirefightingControls>();
+        }
+
+        public static bool wasRegistered = false;
+
+        public static void ensureMainTabRegistered()
+        {
+            if (wasRegistered) { return; }
+
+            Log.Message("Hello from FirefightingControls_RegisterToMainTab ensureMainTabRegistered");
+
+            List<List<ModButton_Text>> columns = MainTabWindow_ModButtons.columns;
+
+            List<ModButton_Text> buttons = new List<ModButton_Text>();
+
+            buttons.Add(new ModButton_Text(
+                delegate
+                {
+                    string buttonLabel = "Firefighting is Currently:" + Environment.NewLine;
+                    if (FirefightingUtilities.getIsEmergency())
+                    {
+                        buttonLabel += "EMERGENCY";
+                    }
+                    else
+                    {
+                        buttonLabel += "Normal Work";
+                    }
+                    return buttonLabel;
+                },
+                delegate {
+                    FirefightingUtilities.setIsEmergency(!FirefightingUtilities.getIsEmergency());
+                }
+            ));
+            buttons.Add(new ModButton_Text(
+                delegate
+                {
+                    return "Firefighting Area is Currently:" + Environment.NewLine + (getComponent().areaToFirefight?.Label ?? "Unrestricted");
+                },
+                delegate {
+                    Find.WindowStack.Add(FirefightingUtilities.getFirefightingAreaMenu());
+                }
+            ));
+
+            columns.Add(buttons);
+
+            wasRegistered = true;
+        }
+    }
+}
---
+++ FirefightingControls/MapComponent_FirefightingControls.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FirefightingControls
+{
+    class MapComponent_FirefightingControls : MapComponent
+    {
+        public Area areaToFirefight;
+        public MapComponent_FirefightingControls(Map map) : base(map)
+        {
+            areaToFirefight = map.areaManager.Home;
+            LongEventHandler.QueueLongEvent(ensureComponentExists, null, false, null);
+        }
+
+        public static void ensureComponentExists()
+        {
+            foreach (Map m in Find.Maps)
+            {
+                if (m.GetComponent<MapComponent_FirefightingControls>() == null)
+                {
+                    m.components.Add(new MapComponent_FirefightingControls(m));
+                }
+            }
+        }
+
+        public bool isInFirefightingArea(IntVec3 position)
+        {
+            if (areaToFirefight == null)
+            {
+                return true;
+            }
+            else
+            {
+                return areaToFirefight[position];
+            }
+        }
+    }
+}
